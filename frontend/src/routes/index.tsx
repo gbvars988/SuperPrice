fc56b1055cb37eac18e4555902edf78c05dafc3e
thomasyao15 <thomasyao15@gmail.com>
2023-08-30 16:48:29 +1000

    feat: connect auth page route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {
-    BrowserRouter,
-    Route,
-    Routes,
-} from 'react-router-dom';
+import {BrowserRouter, Route, Routes,} from 'react-router-dom';
 import HomePage from "../features/home/HomePage";
 import NavBar from "../components/NavBar";
+import AuthPage from "../features/auth/AuthPage";
 
 const Router: React.FC = () => {
     return (
         <BrowserRouter>
-            <NavBar />
+            <NavBar/>
             <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/login" element={<AuthPage/>}/>
                 {/* add more routes later for other features */}
             </Routes>
         </BrowserRouter>
