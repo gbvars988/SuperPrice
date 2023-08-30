65f217eb992f8cb21620dc9b89307872d3651d75
thomasyao15 <thomasyao15@gmail.com>
2023-08-30 15:23:28 +1000

    feat: move color switcher to placeholder navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import {
   Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import Router from "./routes";
 
 export const App = () => (
@@ -13,7 +12,6 @@ export const App = () => (
     <Box textAlign="center" fontSize="xl" >
       <Grid minH="100vh" p={3}>
         {/*Keep the color switcher here for now - refactor into nav bar when that gets created*/}
-        <ColorModeSwitcher justifySelf="flex-end" />
         <Router />
       </Grid>
     </Box>
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { Box, Link } from '@chakra-ui/react';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import {ColorModeSwitcher} from "../ColorModeSwitcher";
 
 const NavBar: React.FC = () => {
     return (
@@ -8,6 +9,7 @@ const NavBar: React.FC = () => {
             <Link as={RouterLink} to="/" mr={4}>
                 Home
             </Link>
+            <ColorModeSwitcher justifySelf="flex-end" />
             {/*style and create more links later - Ledy's ticket*/}
         </Box>
     );
