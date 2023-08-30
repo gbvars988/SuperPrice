5a728ea663c9d62d9e5392cbc78fd935613f8c3a
thomasyao15 <thomasyao15@gmail.com>
2023-08-30 17:14:24 +1000

    test: completed login form tests

---
+++ frontend/src/features/auth/LoginForm.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import LoginForm from './LoginForm';
+import {LABEL} from '../../language';
+
+test('renders LoginForm correctly', () => {
+    render(<LoginForm/>);
+
+    // check all labels and buttons are included in the login form
+    const emailLabel = screen.getByText(LABEL.EMAIL);
+    expect(emailLabel).toBeInTheDocument();
+
+    const passwordLabel = screen.getByText(LABEL.PASSWORD);
+    expect(passwordLabel).toBeInTheDocument();
+
+    const submitButton = screen.getByRole('button', {name: LABEL.LOGIN});
+    expect(submitButton).toBeInTheDocument();
+});
