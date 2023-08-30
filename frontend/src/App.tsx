8bb8e902c9750435c863cc90869654031ae350ea
thomasyao15 <thomasyao15@gmail.com>
2023-08-30 15:19:43 +1000

    feat: display router in app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,11 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import Router from "./routes";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,21 +14,7 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         {/*Keep the color switcher here for now - refactor into nav bar when that gets created*/}
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
+        <Router />
       </Grid>
     </Box>
   </ChakraProvider>
