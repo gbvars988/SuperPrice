1bef46cefd645950c54b2b262b9a29137d1b96ed
thomasyao15 <thomasyao15@gmail.com>
2023-08-31 10:50:48 +1000

    feat: implemented filter component with checkboxes

--- frontend/src/features/shop/ShopPage.tsx
+++ frontend/src/features/shop/ShopPage.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Heading, HStack, VStack} from '@chakra-ui/react';
-import PageContainer from "../../components/common/PageContainer";
+import PageContainer from '../../components/common/PageContainer';
+import Categories from './Categories';
+import {LABEL} from '../../language';
+import Filter from "./Filter";
 
 const ShopPage: React.FC = () => {
+    const initialFilterState = Object.fromEntries(
+        Object.values(Categories).map(category => [category, false])
+    ) as Record<string, boolean>;
+
+    const [filterState, setFilterState] = useState<Record<string, boolean>>(initialFilterState);
+
+    // TODO: use filterState to filter products later once product data is available
 
     return (
         <PageContainer>
@@ -10,28 +20,25 @@ const ShopPage: React.FC = () => {
 
                 <Box w={"100%"} textAlign={"left"}>
                     <Heading as="h2" size="xl">
-                        Shop
+                        {LABEL.SHOP}
                     </Heading>
                 </Box>
 
-                <HStack spacing={8} align={"stretch"} w={"100%"}>
+                <HStack spacing={12} align={"stretch"} w={"100%"}>
 
                     <Box w={"25%"}>
-                        <Heading as="h4" size="md" textAlign={"left"}>
-                            Filter
-                        </Heading>
-                        <Box bg={"gray.200"} p={4} mt={4}>
-                            Filter Component Placeholder
+                        <Box pr={4}>
+                            <Filter filterState={filterState} setFilterState={setFilterState}/>
                         </Box>
                     </Box>
 
                     {/* product gallery Placeholder */}
                     <Box w={"75%"}>
                         <Heading as="h4" size="md" textAlign={"left"}>
-                            Products
+                            {LABEL.PRODUCTS}
                         </Heading>
                         <Box bg={"gray.200"} p={4} mt={4}>
-                            Product Gallery Placeholder
+                            Product gallery placeholder - add component later
                         </Box>
                     </Box>
 
