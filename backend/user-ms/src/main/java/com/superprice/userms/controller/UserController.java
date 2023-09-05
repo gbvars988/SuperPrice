8a548b4ab08b6daa002b2094aaba00b087b54700
rhrmit <s3436258@student.rmit.edu.au>
2023-09-05 22:48:56 +1000

    add userRegistration api endpoint

--- backend/user-ms/src/main/java/com/superprice/userms/controller/UserController.java
+++ backend/user-ms/src/main/java/com/superprice/userms/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.superprice.userms.controller;
 
+
+import com.superprice.userms.model.User;
+import com.superprice.userms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> userRegistration(@RequestBody User user) {
+        boolean regStatus = userService.userRegistration(user);
+        if (regStatus) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully!");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to register user.");
+        }
+
+    }
 }
--- backend/user-ms/src/main/java/com/superprice/userms/model/User.java
+++ backend/user-ms/src/main/java/com/superprice/userms/model/User.java
@@ -1,4 +1,25 @@
 package com.superprice.userms.model;
 
 public class User {
+    private int userID;
+    private String email;
+    private String password;
+
+    public User(int userID, String email, String password) {
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
