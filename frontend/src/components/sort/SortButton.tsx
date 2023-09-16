709031791decf824424cb929725ba52536c8c808
Ledy (s3409664) <s3409664@student.rmit.edu.au>
2023-09-16 13:44:37 +1000

    update sort to use Select component

--- frontend/src/components/sort/SortButton.tsx
+++ frontend/src/components/sort/SortButton.tsx
@@ -1,22 +1,13 @@
 import React from "react";
-import {
-    Button,
-    Menu, MenuButton, MenuItem, MenuList,
-} from "@chakra-ui/react";
-import {ChevronDownIcon} from "@chakra-ui/icons";
+import { Select} from "@chakra-ui/react";
 import {LABEL} from "../../language";
 
 const SortButton = () => {
     return (
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                {LABEL.SORT}
-            </MenuButton>
-            <MenuList>
-                <MenuItem>{LABEL.NAME}</MenuItem>
-                <MenuItem>{LABEL.PRICE}</MenuItem>
-            </MenuList>
-        </Menu>
+    <Select placeholder={LABEL.SORT} maxW={100} >
+        <option value='apiCallSortByName'>{LABEL.NAME}</option>
+        <option value='apiCallSortByPrice'>{LABEL.PRICE}</option>
+    </Select>
     );
 };
 
