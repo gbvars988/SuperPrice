820f6f3a0975788f9657eda81ceabba89c9c0b5b
rhrmit <s3436258@student.rmit.edu.au>
2023-09-17 17:47:53 +1000

    test ci 18, adds docker service container for github actions mysql

--- backend/product-ms/src/main/java/com/superprice/productms/controller/ProductController.java
+++ backend/product-ms/src/main/java/com/superprice/productms/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("products")
 public class ProductController {
+    //
     private final ProductService productService;
     @Autowired
     public ProductController(ProductService productService) {
