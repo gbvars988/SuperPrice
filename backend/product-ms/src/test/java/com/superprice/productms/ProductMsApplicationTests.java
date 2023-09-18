6cdb3344d2ee1f2809390bd8c7b950f05cdfbc60
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2023-09-18 12:49:09 +1000

    fixing the tests as unable to build project locally

--- backend/product-ms/src/test/java/com/superprice/productms/ProductMsApplicationTests.java
+++ backend/product-ms/src/test/java/com/superprice/productms/ProductMsApplicationTests.java
@@ -2,9 +2,10 @@ package com.superprice.productms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
 
 //@SpringBootTest
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 class ProductMsApplicationTests {
 
 	@Test
