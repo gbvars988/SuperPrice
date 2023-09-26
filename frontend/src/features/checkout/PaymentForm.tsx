13916587f408a84368d21c8997173ba0a44f4cfb
thomasyao15 <thomasyao15@gmail.com>
2023-09-26 17:03:14 +1000

    feat: add interactive credit card library

--- frontend/src/features/checkout/PaymentForm.tsx
+++ frontend/src/features/checkout/PaymentForm.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   FormControl,
   FormLabel,
+  Grid,
   Input,
   InputGroup,
   InputLeftElement,
@@ -11,17 +12,34 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { FaCalendarAlt, FaRegCreditCard, FaUserAlt } from "react-icons/fa";
+import Cards, { Focused } from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
 
 const PaymentForm: React.FC = () => {
-  const [cardName, setCardName] = useState("");
-  const [cardNumber, setCardNumber] = useState("");
-  const [validThru, setValidThru] = useState("");
-  const [cvv, setCvv] = useState("");
+  const [state, setState] = useState({
+    name: "",
+    number: "",
+    expiry: "",
+    cvc: "",
+    focus: "",
+  });
+
+  const handleInputChange = (evt: any) => {
+    const { name, value } = evt.target;
+    setState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleInputFocus = (evt: any) => {
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     console.log("submitting payment form");
   };
+
+  const allFieldsFilled =
+    state.name && state.number && state.expiry && state.cvc;
 
   return (
     <Box w={"80%"}>
@@ -29,8 +47,8 @@ const PaymentForm: React.FC = () => {
 
       <Box borderWidth={1} borderRadius="md" p={10} minWidth={"450px"}>
         <FormControl onSubmit={handleSubmit}>
-          <Box display="flex" justifyContent="space-between" mb={7}>
-            <Box w="48%">
+          <Grid templateColumns="1fr 1fr" gap={6} mb={7}>
+            <Box>
               <FormLabel>Cardholder's Name</FormLabel>
               <InputGroup>
                 <InputLeftElement
@@ -39,14 +57,15 @@ const PaymentForm: React.FC = () => {
                 />
                 <Input
                   type="text"
+                  name="name"
                   placeholder="John Doe"
-                  value={cardName}
-                  onChange={(e) => setCardName(e.target.value)}
+                  value={state.name}
+                  onChange={handleInputChange}
+                  onFocus={handleInputFocus}
                 />
               </InputGroup>
-            </Box>
-            <Box w="48%">
-              <FormLabel>Card Number</FormLabel>
+
+              <FormLabel mt={5}>Card Number</FormLabel>
               <InputGroup>
                 <InputLeftElement
                   pointerEvents="none"
@@ -54,50 +73,75 @@ const PaymentForm: React.FC = () => {
                 />
                 <Input
                   type="text"
+                  name="number"
                   placeholder="1234 5678 9123 4567"
-                  value={cardNumber}
-                  onChange={(e) => setCardNumber(e.target.value)}
+                  value={state.number}
+                  onChange={handleInputChange}
+                  onFocus={handleInputFocus}
                 />
               </InputGroup>
-            </Box>
-          </Box>
 
-          <Box display="flex" justifyContent="space-between" mb={7}>
-            <Box w="24%">
-              <FormLabel>Valid Thru (MM/YY)</FormLabel>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents="none"
-                  children={<FaCalendarAlt color="gray.300" />}
-                />
-                <Input
-                  type="text"
-                  placeholder="08/25"
-                  value={validThru}
-                  onChange={(e) => setValidThru(e.target.value)}
-                />
-              </InputGroup>
+              <Grid templateColumns="repeat(2, 1fr)" gap={6} mb={3} mt={7}>
+                <Box>
+                  <FormLabel>Valid Thru</FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<FaCalendarAlt color="gray.300" />}
+                    />
+                    <Input
+                      type="text"
+                      name="expiry"
+                      placeholder="08/25"
+                      value={state.expiry}
+                      onChange={handleInputChange}
+                      onFocus={handleInputFocus}
+                    />
+                  </InputGroup>
+                </Box>
+
+                <Box>
+                  <FormLabel>CVV/CVC</FormLabel>
+                  <Input
+                    type="text"
+                    name="cvc"
+                    placeholder="123"
+                    value={state.cvc}
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                  />
+                </Box>
+              </Grid>
+
+              <Box display="flex" alignItems="center">
+                <Text fontSize="xs" color="gray.500">
+                  * CVV or CVC is the card security code on the back of your
+                  card, separate from its number.
+                </Text>
+              </Box>
             </Box>
-            <Box w="24%">
-              <FormLabel>CVV/CVC</FormLabel>
-              <Input
-                type="text"
-                placeholder="123"
-                value={cvv}
-                onChange={(e) => setCvv(e.target.value)}
+
+            <Box>
+              <Cards
+                number={state.number}
+                expiry={state.expiry}
+                cvc={state.cvc}
+                name={state.name}
+                focused={state.focus as Focused}
               />
             </Box>
-            <Box w="48%" display="flex" alignItems="center">
-              <Text fontSize="xs" color="gray.500">
-                * CVV or CVC is the card security code on the back of your card
-                separate from its number.
-              </Text>
-            </Box>
-          </Box>
+          </Grid>
 
-          <Button type="submit" onClick={handleSubmit}>
-            Confirm Payment
-          </Button>
+          <Box display="flex" justifyContent="center">
+            <Button
+              colorScheme="teal"
+              type="submit"
+              onClick={handleSubmit}
+              isDisabled={!allFieldsFilled}
+            >
+              Confirm Payment
+            </Button>
+          </Box>
         </FormControl>
       </Box>
     </Box>
