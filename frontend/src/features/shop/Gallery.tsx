720501f33d27b96b4adcea406d5298bfd3780b6a
dyl-kh <dylanjkhan@hotmail.com>
2023-09-27 19:47:27 +1000

    sort product gallery based on sort state

--- frontend/src/features/shop/Gallery.tsx
+++ frontend/src/features/shop/Gallery.tsx
@@ -2,30 +2,73 @@ import React from "react";
 import { Box, Heading, useColorModeValue, Flex } from "@chakra-ui/react";
 import { LABEL } from "../../language";
 import VProductCard from "./VProductCard";
+import SortOptions from "./SortOptions";
 
 interface GalleryProps {
   products: any[];
   filterState: Record<string, boolean>;
+  sortState: string;
 }
+
+type product = {
+  productID: string;
+  name: string;
+  weight: number;
+  imageURL: string;
+  supermarketPrices: {
+    supermarketId: number;
+    supermarketName: string;
+    price: number;
+  }[];
+  category: string;
+  description: string;
+};
+
+const getCheapestPrice = (product: product) => {
+  return Math.min(...product.supermarketPrices.map((p) => p.price));
+};
 
 const Gallery = (props: GalleryProps) => {
   React.useEffect(() => {
-    console.log(displayProducts)
+    console.log(displayProducts);
   });
 
   // Determine which products to display
   const displayProducts = React.useMemo(() => {
     // Extract categories that are true from filterState
-    const activeFilters = Object.keys(props.filterState).filter(key => props.filterState[key]);
+    const activeFilters = Object.keys(props.filterState).filter(
+      (key) => props.filterState[key],
+    );
 
-    // If no filters are active, or all filters are active, display all products
-    if (activeFilters.length === 0 || activeFilters.length === Object.keys(props.filterState).length) {
-      return props.products;
+    // Initial product filter logic
+    let filteredProducts = props.products;
+    if (
+      activeFilters.length > 0 &&
+      activeFilters.length < Object.keys(props.filterState).length
+    ) {
+      filteredProducts = props.products.filter((product) =>
+        activeFilters.includes(product.category),
+      );
     }
 
-    // Filter products that match the active filters
-    return props.products.filter(product => activeFilters.includes(product.category));
-  }, [props.products, props.filterState]);
+    // Sorting logic based on sortState
+    switch (props.sortState) {
+      case SortOptions.NAME_ASC:
+        return filteredProducts.sort((a, b) => a.name.localeCompare(b.name));
+      case SortOptions.NAME_DESC:
+        return filteredProducts.sort((a, b) => b.name.localeCompare(a.name));
+      case SortOptions.PRICE_ASC:
+        return filteredProducts.sort(
+          (a, b) => getCheapestPrice(a) - getCheapestPrice(b),
+        );
+      case SortOptions.PRICE_DESC:
+        return filteredProducts.sort(
+          (a, b) => getCheapestPrice(b) - getCheapestPrice(a),
+        );
+      default:
+        return filteredProducts;
+    }
+  }, [props.products, props.filterState, props.sortState]);
 
   return (
     <Box w={"75%"}>
