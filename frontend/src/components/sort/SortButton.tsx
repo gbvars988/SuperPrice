4d5daa28f479edc34be95b3b658fff059fb94e26
dyl-kh <dylanjkhan@hotmail.com>
2023-09-27 19:11:40 +1000

    update sort button to use options

--- frontend/src/components/sort/SortButton.tsx
+++ frontend/src/components/sort/SortButton.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Select} from "@chakra-ui/react";
 import {LABEL} from "../../language";
+import SortOptions from "../../features/shop/SortOptions";
 
 const SortButton = () => {
     return (
-    <Select placeholder={LABEL.SORT} maxW={100} >
-        <option value='apiCallSortByName'>{LABEL.NAME}</option>
-        <option value='apiCallSortByPrice'>{LABEL.PRICE}</option>
+    <Select placeholder={LABEL.SORT} maxW={150} >
+        {Object.values(SortOptions).map((option, i) => (
+            <option key={i} value={option}>{LABEL[option]}</option>
+        ))}
     </Select>
     );
 };
