bc942cf919f17ff731b26b4dd2d5b98dbc8604b8
Ledy (s3409664) <s3409664@student.rmit.edu.au>
2023-09-27 20:05:11 +1000

    create user review form component

---
+++ frontend/src/components/review/ReviewForm.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  Button,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+interface ReviewFormProps {
+  onSubmit: (data: { name: string; review: string }) => void;
+}
+
+const ReviewForm: React.FC<ReviewFormProps> = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({ name: "", review: "" });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(formData);
+    setFormData({ name: "", review: "" });
+  };
+
+  return (
+    <Box maxW="md" mx="auto">
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={4}>
+          <FormControl id="name" isRequired>
+            <FormLabel>Name</FormLabel>
+            <Input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <FormControl id="review" isRequired>
+            <FormLabel>Review</FormLabel>
+            <Textarea
+              name="review"
+              value={formData.review}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            colorScheme={useColorModeValue("blackAlpha", "whiteAlpha")}
+            bg={useColorModeValue("black", "white")}
+          >
+            Submit Review
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
+
+export default ReviewForm;
