a77ab58d526cf0804e8b71cadabb04f3ec27c47b
thomasyao15 <thomasyao15@gmail.com>
2023-10-04 19:36:45 +1100

    style: refine card styling with badge colour

--- frontend/src/features/deliveries/DeliveryCard.tsx
+++ frontend/src/features/deliveries/DeliveryCard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Badge, Box, Text, VStack } from "@chakra-ui/react";
 
 interface DeliveryCardProps {
   orderId: number;
@@ -17,7 +17,6 @@ const DeliveryCard: React.FC<DeliveryCardProps> = ({ orderId }) => {
           `http://localhost:8082/delivery-service/delivery/getdeliverybyid/${orderId}`,
         );
         setDelivery(response.data);
-        console.log("Delivery", response.data);
       } catch (error) {
         console.error("Error fetching delivery details", error);
       }
@@ -27,14 +26,12 @@ const DeliveryCard: React.FC<DeliveryCardProps> = ({ orderId }) => {
   }, [orderId]);
 
   useEffect(() => {
-    console.log("Fetching timeslot", delivery.timeSlotId);
     const fetchTimeslot = async () => {
       if (!delivery.timeSlotId) return;
       try {
         const response = await axios.get(
           `http://localhost:8082/delivery-service/delivery/timeslots/${delivery.timeSlotId}`,
         );
-        console.log("Timeslot", response.data);
         setTimeslot(response.data);
       } catch (error) {
         console.error("Error fetching timeslot", error);
@@ -45,15 +42,45 @@ const DeliveryCard: React.FC<DeliveryCardProps> = ({ orderId }) => {
   }, [delivery]);
 
   return (
-    <Box borderWidth="1px" borderRadius="lg" padding="6" width="full">
-      <VStack spacing={4} align="start">
-        <Text>Order ID: {delivery.orderId}</Text>
-        <Text>Address: {delivery.address}</Text>
-        <Text>Email: {delivery.email}</Text>
+    <Box
+      borderWidth="1px"
+      borderRadius="lg"
+      padding="6"
+      width="full"
+      boxShadow="md"
+    >
+      <VStack spacing={3} align="start">
         <Text>
-          Timeslot: {timeslot.startTime} - {timeslot.endTime}
+          <Text as="span" fontWeight="semibold">
+            Order ID:
+          </Text>{" "}
+          {delivery.orderId}
         </Text>
-        <Text>Delivery Status: {delivery.deliveryStatus}</Text>
+        <Text>
+          <Text as="span" fontWeight="semibold">
+            Address:
+          </Text>{" "}
+          {delivery.address}
+        </Text>
+        <Text>
+          <Text as="span" fontWeight="semibold">
+            Email:
+          </Text>{" "}
+          {delivery.email}
+        </Text>
+        <Text>
+          <Text as="span" fontWeight="semibold">
+            Timeslot:
+          </Text>{" "}
+          {timeslot.startTime} - {timeslot.endTime}
+        </Text>
+        <Badge
+          colorScheme={
+            delivery.deliveryStatus === "Delivered" ? "green" : "orange"
+          }
+        >
+          {delivery.deliveryStatus}
+        </Badge>
       </VStack>
     </Box>
   );
